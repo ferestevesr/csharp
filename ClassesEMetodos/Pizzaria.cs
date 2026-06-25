f5e49340050faa598e0b74a5de540dc601df8146
ferestevesr <fernandovinirocha@gmail.com>
2026-06-25 16:26:52 -0300

    Atualização

--- ClassesEMetodos/Pizzaria.cs
+++ ClassesEMetodos/Pizzaria.cs
@@ -6,5 +6,45 @@ using System.Threading.Tasks;
 
 namespace CursoCSharp.ClassesEMetodos {
     internal class Pizzaria {
-    }
+
+            public static void MontarPizza(params string[] ingredientes) {
+                Console.WriteLine("\nSua pizza terá " + ingredientes.Length + " ingredientes:");
+
+                foreach (string ingrediente in ingredientes) {
+                    Console.WriteLine("- " + ingrediente);
+                }
+            }
+
+            public static void Executar() {
+                List<string> ingredientes = new List<string>();
+
+                Console.WriteLine("Digite os ingredientes da pizza:");
+                Console.WriteLine("Digite 'fim' para finalizar.");
+
+                while (true) {
+                    Console.Write("Ingrediente: ");
+                    string ingrediente = Console.ReadLine();
+
+                    if (ingrediente.ToLower() == "fim") {
+                        break;
+                    }
+
+                    if (ingrediente == "") {
+                        Console.WriteLine("Ingrediente inválido! Digite novamente.");
+                        continue;
+                    }
+
+                    ingredientes.Add(ingrediente);
+                }
+
+                if (ingredientes.Count == 0) {
+                    Console.WriteLine("Nenhum ingrediente foi adicionado.");
+                    return;
+                }
+
+                MontarPizza(ingredientes.ToArray());
+            }
+        }
 }
+
+
